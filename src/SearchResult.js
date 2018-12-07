50620958d626544ee3d3fb0953553944e5425a42
Tasnuva Noor <noortasnuva@gmail.com>
2018-12-07 09:41:32 -0500

    fixed a bug in filter

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,49 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Gif from './Gif.js'
 import Buttons from './Buttons.js'
 import { getSorted, getFiltered } from './utils'
 
 
-class SearchResult extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filterType: null,
-    }
-    this.onFilterClick = this.onFilterClick.bind(this)
-    this.clearFilter = this.clearFilter.bind(this)
-  }
-
-  onFilterClick(type) {
-    this.setState({filterType: type})
-  }
-
-  clearFilter() {
-    this.setState({filterType: null})
-  }
-
-  render() {
-    const { message, sortData, onSortClick } = this.props
-    const results = sortData ? getSorted(this.props.results) : this.props.results
-    const { filterType } = this.state
+const SearchResult = (props) => {
+  const { message, sortData, onSortClick, filterType, onFilterClick, clearFilter  } = props
+    const results = sortData ? getSorted(props.results) : props.results
     
-    return (
-      <div>
-        <p className="message">{message}</p>
-        <Buttons onSortClick={onSortClick} onFilterClick={this.onFilterClick} clearFilter={this.clearFilter}/>
-        <div className="result">
-          {
-            filterType
-            ? (
-                getFiltered(filterType, results).length !== 0 
-                ? getFiltered(filterType, results).map(result => <Gif result={result} key={result.id}/>) : <p>Sorry! There's no gif matching your criteria.</p>
-              )
-            : results.map(result => <Gif result={result} key={result.id}/>)
-          }
-        </div>
+  return (
+    <div>
+      <p className="message">{message}</p>
+      <Buttons onSortClick={onSortClick} onFilterClick={onFilterClick} clearFilter={clearFilter}/>
+      <div className="result">
+        {
+          filterType
+          ? (
+              getFiltered(filterType, results).length !== 0 
+              ? getFiltered(filterType, results).map(result => <Gif result={result} key={result.id}/>) : <p>Sorry! There's no gif matching your criteria.</p>
+            )
+          : results.map(result => <Gif result={result} key={result.id}/>)
+        }
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default SearchResult;
